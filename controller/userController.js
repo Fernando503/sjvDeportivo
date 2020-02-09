b02e8942a5323fc8e9a43710dfe5510ad9b7ca24
Fernando Flamenco <l.fernando.flamenco@gmail.com>
2020-02-09 17:40:56 -0600

    add function of update and delete the users

--- controller/userController.js
+++ controller/userController.js
@@ -6,40 +6,40 @@ const jwt = require('jsonwebtoken')
 const { SECRET_TOKEN } = require('../config/generalConfig')
 const fecha = require('../utils')
 
-const saveUser = async (req,res)=>{
-	try{
-		const { body } = req
-		const user = new userModel()
-		user.username 			= body.usuario
-		user.email    			= body.email
-		user.password			= body.password
-		user.fullName			= body.fullName
-		user.rol				= body.rol
-		user.dateCreation		= body.create
-		const registro 			= await user.save()
+const saveUser = async (req, res) => {
+    try {
+        const { body } = req
+        const user = new userModel()
+        user.username = body.usuario
+        user.email = body.email
+        user.password = body.password
+        user.fullName = body.fullName
+        user.rol = body.rol
+        user.dateCreation = body.create
+        const registro = await user.save()
 
 
-		console.log('Registro creado: '+ registro)
-		return res.status(200).json({code: '00', message: "Usuario creado exitosamente"})
+        console.log('Registro creado: ' + registro)
+        return res.status(200).json({ code: '00', message: "Usuario creado exitosamente" })
 
-	}catch(err){
+    } catch (err) {
         console.log(err)
-        if(err.code == 11000){
-                var cadena = err.errmsg
-                var termino = "email_1"
-                var posicion = cadena.toLowerCase().indexOf(termino.toLowerCase());
-                if (posicion !== -1)
-                    return res.status(500).json({code: err.code, message: `El Correo electronico ingresado ya existe`})
+        if (err.code == 11000) {
+            var cadena = err.errmsg
+            var termino = "email_1"
+            var posicion = cadena.toLowerCase().indexOf(termino.toLowerCase());
+            if (posicion !== -1)
+                return res.status(500).json({ code: err.code, message: `El Correo electronico ingresado ya existe` })
 
-                return res.status(500).json({code: err.code, message: `El Usuario ingresado ya existe`})
-            } else {
-                return res.status(500).json({code: 12000, message: `El tipo de usuario ingresado no es permitido`})
-            }
+            return res.status(500).json({ code: err.code, message: `El Usuario ingresado ya existe` })
+        } else {
+            return res.status(500).json({ code: 12000, message: `El tipo de usuario ingresado no es permitido` })
+        }
 
-            
-                
-		return res.status(500).json({message: `Error al guardar usuario`, data:err.code})
-	}
+
+
+        return res.status(500).json({ message: `Error al guardar usuario`, data: err.code })
+    }
 }
 
 const LocalStrategy = require('passport-local').Strategy;
@@ -51,7 +51,7 @@ passport.use('local_login', new LocalStrategy({
     (username, password, done) => {
         try {
             var fechaActual = ''
-            userModel.findOne({ username: username }, function callback(error, user) {
+            userModel.findOne({ username: username, status: 'Activo' }, function callback(error, user) {
                 if (!user)
                     return done(null, false, { message: 'Usuario no encontrado' });
 
@@ -71,16 +71,16 @@ passport.use('local_login', new LocalStrategy({
                         }
 
                         const token = jwt.sign(payload, SECRET_TOKEN, {
-                            expiresIn: 60*30
+                            expiresIn: 60 * 30
                         })
-                       
+
                         fechaActual = fecha.hoyFecha()
                         //console.log(fechaActual)
                         userModel.updateOne({ "_id": user._id }, { $set: { lastLogin: fechaActual } }, (err, todo) => {
                             // Handle any possible database errors
                             if (err) return done(null, false, { message: err });
 
-                            return done(null, user, { message: 'Login exitoso', token: token});
+                            return done(null, user, { message: 'Login exitoso', token: token });
                         })
 
                     } else {
@@ -89,7 +89,7 @@ passport.use('local_login', new LocalStrategy({
                 })
             })
         } catch (error) {
-        	console.log(error)
+            console.log(error)
             return done(null, false, { message: "Se produjo un error desconocido, contacte al Administrador" });
         }
     }
@@ -97,48 +97,134 @@ passport.use('local_login', new LocalStrategy({
 
 
 
-function getUsuarioByUsername(req,res){
-	userModel.findOne({username: req.query.username}, function callback(error, user){
-		if (!user)
-			return res.status(404).json({message: "No se encontró el usuario"})
+function getUsuarioByUsername(req, res) {
+    userModel.findOne({ username: req.query.username }, function callback(error, user) {
+        if (!user)
+            return res.status(404).json({ message: "No se encontró el usuario" })
 
-		const DatosUser = {
-				usuario 		: user.username,
-				email			: user.email,
-				fullName		: user.fullName,
-				rol				: user.rol,
-				UltimaSesion 	: user.lastLogin
-			}
+        const DatosUser = {
+            usuario: user.username,
+            email: user.email,
+            fullName: user.fullName,
+            rol: user.rol,
+            UltimaSesion: user.lastLogin
+        }
 
-		return res.status(200).json({code: '00', message: DatosUser})
-	})
-}
-
-function getAllUsers(req,res){
-    userModel.find({status: 'Activo'}, function (erros, users){
-        if (!users)
-            return res.status(404).json({message: "No se encontraron usuarios"})
-
-        var lstUser = []
-        users.forEach(function (currentValue, index, user){
-            const DatosUser = {
-                usuario         : user[index].username,
-                email           : user[index].email,
-                fullName        : user[index].fullName,
-                rol             : user[index].rol,
-                UltimaSesion    : user[index].lastLogin
-            }
-            lstUser.push(DatosUser)
-        })
-        
-
-        return res.status(200).json({code: '00', message: lstUser})
-
+        return res.status(200).json({ code: '00', message: DatosUser })
     })
 }
 
+function getAllUsers(req, res) {
+    userModel.find({ status: 'Activo' }, function(erros, users) {
+        if (!users)
+            return res.status(404).json({ message: "No se encontraron usuarios" })
+
+        var lstUser = []
+        users.forEach(function(currentValue, index, user) {
+            const DatosUser = {
+                usuario: user[index].username,
+                email: user[index].email,
+                fullName: user[index].fullName,
+                rol: user[index].rol,
+                UltimaSesion: user[index].lastLogin
+            }
+            lstUser.push(DatosUser)
+        })
+        return res.status(200).json({ code: '00', message: lstUser })
+    })
+}
+
+function updateUser(req, res) {
+    try {
+        var per = req.decoded.Usuario.permisos
+        switch (per) {
+            case 'Administrador':
+                userModel.updateOne({ username: req.body.username }, {
+                    $set: {
+                        fullName: req.body.fullName,
+                        rol: req.body.rol,
+                        status: req.body.status
+                    }
+                }, (err, updateResponse) => {
+                    if (err)
+                        return res.status(500).json({ code: '01', message: 'Error al actualizar datos del usuario' })
+
+                    console.log(updateResponse)
+                    return res.status(200).json({ code: '00', message: 'Se actualizarón los datos correctamente' })
+                })
+                break
+            case 'AdminClient':
+                userModel.updateOne({ username: req.body.username }, {
+                    $set: {
+                        fullName: req.body.fullName,
+                        rol: req.body.rol,
+                        status: req.body.status
+                    }
+                }, (err, updateResponse) => {
+                    if (err)
+                        return res.status(500).json({ code: '01', message: 'Error al actualizar datos del usuario' })
+
+                    console.log(updateResponse)
+                    return res.status(200).json({ code: '00', message: 'Se actualizarón los datos correctamente' })
+                })
+                break
+            case 'Seguidor':
+                userModel.updateOne({ username: req.body.username }, {
+                    $set: {
+                        fullName: req.body.fullName
+                    }
+                }, (err, updateResponse) => {
+                    if (err)
+                        return res.status(500).json({ code: '01', message: 'Error al actualizar datos del usuario' })
+
+                    console.log(updateResponse)
+                    return res.status(200).json({ code: '00', message: 'Se actualizarón los datos correctamente' })
+                })
+                break
+            case 'Usuario':
+                userModel.updateOne({ username: req.body.username }, {
+                    $set: {
+                        fullName: req.body.fullName
+                    }
+                }, (err, updateResponse) => {
+                    if (err)
+                        return res.status(500).json({ code: '01', message: 'Error al actualizar datos del usuario' })
+
+                    console.log(updateResponse)
+                    return res.status(200).json({ code: '00', message: 'Se actualizarón los datos correctamente' })
+                })
+                break
+        }
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json({ code: '01', message: 'Error al Actualizar usuario' })
+    }
+}
+
+function deleteUser(req, res) {
+    try {
+        var per = req.decoded.Usuario.permisos
+        if (per == 'Administrador') {
+            userModel.updateOne({ username: req.body.username }, { $set: { status: 'Desactivado' } }, (err, todo) => {
+                if (err)
+                    return res.status(500).json({ code: '01', message: `Error al Eliminar el usuario: ${req.body.username}` })
+
+                return res.status(200).json({ code: '00', message: 'Usuario eliminado correctamente' })
+            })
+        } else {
+            return res.status(401).json({ code: '401', message: 'No posees autorización para realizar la acción' })
+        }
+
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json({ code: '01', message: 'Error al Eliminar usuario' })
+    }
+}
+
 module.exports = {
-	saveUser,
-	getUsuarioByUsername,
-    getAllUsers
+    saveUser,
+    getUsuarioByUsername,
+    getAllUsers,
+    updateUser,
+    deleteUser
 }
--- rutas/rutas.js
+++ rutas/rutas.js
@@ -5,13 +5,12 @@ const userController = require('../controller/userController')
 const passport = require('passport')
 const secury = require('../middleware/rutasProtegidas')
 
-routers.get('/', (req, res) =>{
-	res.status(200).send('<h1><center>Hello SJV</center></h1>')
-
+routers.get('/', (req, res) => {
+    res.status(200).send('<h1><center>Hello SJV</center></h1>')
 })
 
 routers.post('/auth', (req, res, next) => {
-	
+
     passport.authenticate('local_login', { session: false },
         (err, user, info) => {
             //INICIA UNA VARIABLE DE SESION PARA EL LOGIN, PERO EN ESTE CASO CREAEL JWT
@@ -27,6 +26,8 @@ routers.post('/auth', (req, res, next) => {
 routers.post('/user/add', userController.saveUser)
 routers.get('/user/getUser', secury.vJWT, userController.getUsuarioByUsername)
 routers.get('/users', secury.vJWT, userController.getAllUsers)
+routers.put('/user/update', secury.vJWT, userController.updateUser)
+routers.delete('/user/delete', secury.vJWT, userController.deleteUser)
 
 
 module.exports = routers
