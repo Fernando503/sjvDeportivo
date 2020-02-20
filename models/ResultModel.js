db5d8e2652cdbccedbdca46a929348e36d967bff
Fernando Flamenco <l.fernando.flamenco@gmail.com>
2020-02-19 20:20:35 -0600

    create model of Resultado

--- models/ResultModel.js
+++ models/ResultModel.js
@@ -4,7 +4,21 @@ const Schema = mongoose.Schema
 const fecha = require('../utils')
 
 const ResultadoSchema = Schema({
-	
+	jornada: String,
+	fecha: Date,
+	categoria: String,
+	equipoA: [{
+		tarjetas: Schema.Types.Mixed,
+		resultado: Number,
+		goleadores: Schema.Types.Mixed,
+		status: String
+	}],
+	equipoB: [{
+		tarjetas: Schema.Types.Mixed,
+		resultado: Number,
+		goleadores: Schema.Types.Mixed,
+		status: String
+	}],
 	dateCreation: [{
 		creation: { type: Date, default: fecha.hoyFecha() },
 		createBy: { type: String }
